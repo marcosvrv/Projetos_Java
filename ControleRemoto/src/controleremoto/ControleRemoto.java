010245fcd2fd3ead67c01793be2107b50b47a7e4
Marcos Vinícyus Ramos Vieira <marcos.ramos15@outlook.com>
2024-10-01 14:35:46 -0300

    Projeto ControleRemoto

---
+++ ControleRemoto/src/controleremoto/ControleRemoto.java
@@ -0,0 +1,15 @@
+package controleremoto;
+
+public class ControleRemoto {
+
+    public static void main(String[] args) {
+        RemoteControl c = new RemoteControl();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu(); 
+        
+        
+    }
+    
+}
